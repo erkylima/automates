0b0aa034a49c32aa13ff06eab49a9b673214796b
Érky Lima <erkylima@gmail.com>
2024-06-12 17:24:12 -0300

    setup open tofu inside server software

--- cmd/rest/main.go
+++ cmd/rest/main.go
@@ -2,22 +2,18 @@ package main
 
 import (
 	tofu_domains "github.com/erkylima/automates/internal/tofu/core/domains"
+	tofu_services "github.com/erkylima/automates/internal/tofu/core/services"
 	"github.com/erkylima/automates/pkg/database"
 )
 
 // repo <- service -> serializer  -> http
 
 func main() {
-	base, err := database.NewMongoConnection[tofu_domains.WorkflowNode]("mongodb://admin:password@localhost:27017/admin?retryWrites=true&loadBalanced=false&connectTimeoutMS=10000&authSource=admin&authMechanism=SCRAM-SHA-1", "tofu", "workflows")
+	base, err := database.NewMongoConnection[tofu_domains.TofuWorkflow]("mongodb://admin:password@localhost:27017/admin?retryWrites=true&loadBalanced=false&connectTimeoutMS=10000&authSource=admin&authMechanism=SCRAM-SHA-1", "tofu", "workflows")
 	if err != nil {
 		panic(err)
 	}
-	work := &tofu_domains.WorkflowNode{
-		Name:        "workflow",
-		Description: "test",
-		Type:        tofu_domains.NodePlan,
-	}
 
-	base.Create(work)
-
+	tofu := tofu_services.NewTofuService(base)
+	tofu.InitTofu()
 }
